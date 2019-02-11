848ea6ec9eb07f1026d0cfdf7924f28b05bb4e77
Sourav776 <souraviit776.bd@gmail.com>
2019-02-11 16:19:30 +0600

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Enter your mail to verify"></asp:Label>
+        <br/>
+        <asp:TextBox ID="Email" runat="server"></asp:TextBox>
+        <asp:Button ID="Check" runat="server" Text="Check" OnClick="Check_Click" />
+        <br/>
+        <asp:Label ID="message_lbl" runat="server" ></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Check_Click(object sender, EventArgs e)
+    {
+        //Checker c;
+        if (Checker.CheckMailBox(Email.Text))
+        {
+            message_lbl.Text = "Email does exist!";
+            message_lbl.ForeColor = System.Drawing.Color.Green;
+        }
+        else
+        {
+            message_lbl.Text = "Invalid Email";
+            message_lbl.ForeColor =System.Drawing.Color.Red;
+        }
+    }
+}
